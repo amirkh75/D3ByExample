1ade975c66f386be68606be12331f3720fadf187
amirkh75 <amirp.1375@gmail.com.com>
2021-12-06 13:14:19 -0500

    final dockerization

--- blog_/Blog/settings.py
+++ blog_/Blog/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','3@hh@apzghectdi2^i+7weiny5ccc2eubub*h)@88c1s3+%ph-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG'))
+DEBUG = int(os.environ.get('DEBUG',0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','*').split(' ')
 
 
 # Application definition
