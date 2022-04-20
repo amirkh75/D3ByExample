da959404be90589eb635eba64a95764ca3891b9e
amirkh75 <amirp.1375@gmail.com>
2022-04-21 01:30:10 +0430

    Resetting password views.

--- bookmark_project/bookmarks/account/urls.py
+++ bookmark_project/bookmarks/account/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('logout/',
         auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+
     # change password urls
     path('password_change/',
         auth_views.PasswordChangeView.as_view(),
@@ -21,4 +22,18 @@ urlpatterns = [
             # template_name='registration/password_reset_form.html',
             # form_class=CustomPasswordResetForm),
         name='password_change_done'),
+
+    # reset password urls
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
 ]
